81c4805b9b93cd1f8d09ed593c3368e94143afbb
Amrish-Sharma <Amrish-Sharma@users.noreply.github.com>
2025-01-30 04:52:14 +0000

    deploy: 8c03fd92455bfa71844a1b32b6187e5448789d35

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ function getRandomQuote() {
         .then(data => {
             const randomIndex = Math.floor(Math.random() * data.length);
             const quote = data[randomIndex];
-            document.getElementById('quote').innerText = quote.text;
+            document.getElementById('quote').innerText = quote.quote;
+            console.log(quote.quote);
             document.getElementById('author').innerText = `— ${quote.author}`;
+            console.log(quote.author);
         })
         .catch(error => console.error('Error fetching quotes:', error));
 }
